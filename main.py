5b240cf2ed018636464d9ec9720c9dd62ee621d6
Ali Yusifov <72607596+aliyusifov99@users.noreply.github.com>
2023-10-22 23:57:21 +0800

    add open ai enquirer class

--- main.py
+++ main.py
@@ -1,8 +1,14 @@
 import streamlit as st
+import ast
 import openai
 import os
 from keys import API_KEY
-from open_ai import OpenAI
+import pandas as pd
+from open_ai_inquirer import OpenAIInquirer
+
+embeddings_path = 'https://raw.githubusercontent.com/aliyusifov99/petrobot/main/petrel_manual.csv'
+df = pd.read_csv(embeddings_path)
+df['embedding'] = df['embedding'].apply(ast.literal_eval)
 
     
 st.markdown("<h1 style='text-align: center;'>PetroBot🛢️</h1>", unsafe_allow_html=True)
@@ -16,8 +22,8 @@ def show_message(txt:str):
     if len(txt)==0:
         print('')    
     else:
-        openai = OpenAI(txt)
-        response = openai.generate_response()
+        inquirer = OpenAIInquirer(text=txt, df=df)
+        response = inquirer.inquire()
         st.write(response)
     
     
